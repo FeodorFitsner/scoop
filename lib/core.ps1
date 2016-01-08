793d94ee424a9b45e3143e00d0e0c3dd85f806a0
Feodor Fitsner <feodor@appveyor.com>
2016-01-08 15:59:28 -0800

    $projectrootpath

--- lib/core.ps1
+++ lib/core.ps1
@@ -2,7 +2,7 @@ $scoopdir = $env:SCOOP, "~\appdata\local\scoop" | select-object -first 1
 $globaldir = $env:SCOOP_GLOBAL, "$($env:programdata.tolower())\scoop" | select-object -first 1
 
 $projectrootpath = $null
-if ($MyInvocation.mycommand.Definition) { $projectrootpath = (Split-Path -Parent $MyInvocation.MyCommand.Definition) }
+if ($MyInvocation.mycommand.Definition) { $projectrootpath = ($MyInvocation.MyCommand.Definition | Split-Path | Split-Path) }
 
 function rootrelpath($path) { join-path $projectrootpath $path } # relative to project main directory
 
