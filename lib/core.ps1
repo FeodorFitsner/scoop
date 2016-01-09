a5c12eef90712f159c64a23bdd0b5e3242ba6bd5
Feodor Fitsner <feodor@appveyor.com>
2016-01-08 16:06:01 -0800

    Update core.ps1

--- lib/core.ps1
+++ lib/core.ps1
@@ -25,7 +25,7 @@ function ConvertFrom-JsonPoSH2 {
     BEGIN {
         if (-not (Get-Module 'Newtonsoft.Json')) {
             $modulePath = rootrelpath('vendor\Newtonsoft.Json\lib\net20\Newtonsoft.Json.dll')
-            $modulePath
+            Write-Host "Module path: $modulePath"
             import-module $modulePath
         }
         $f_ToObject = { param( $token )
