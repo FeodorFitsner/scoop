c840582422c2481177fdcd3fca7277495c9f7d5d
Feodor Fitsner <feodor@appveyor.com>
2016-01-08 16:01:01 -0800

    Another attempt $modulePath

--- lib/core.ps1
+++ lib/core.ps1
@@ -21,9 +21,9 @@ function ConvertFrom-JsonPoSH2 {
         )
     BEGIN {
         if (-not (Get-Module 'Newtonsoft.Json')) {
-            $modulePath = $(resolve-path $(rootrelpath 'vendor\Newtonsoft.Json\lib\net20\Newtonsoft.Json.dll'))
+            $modulePath = (rootrelpath 'vendor\Newtonsoft.Json\lib\net20\Newtonsoft.Json.dll')
             $modulePath
-            import-module $(resolve-path $(rootrelpath 'vendor\Newtonsoft.Json\lib\net20\Newtonsoft.Json.dll'))
+            import-module $modulePath
         }
         $f_ToObject = { param( $token )
             $type = $token.psobject.TypeNames -imatch "Newtonsoft\..*(JObject|JArray|JProperty|JValue)"
